97240b557efd32fdac578d2ac44c3e13fc29f449
You Song Hou <yousonghou@yahoo.com>
2020-08-08 00:05:29 -0400

    add a script to move the shield

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public float speed = 15f;
+    public float mapHeight = 5f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Move the shield
+    void FixedUpdate()
+    {
+        float y = Input.GetAxis("Vertical") * Time.fixedDeltaTime * speed;
+
+        Vector2 newPosition = rb.position + Vector2.up * y;
+        newPosition.y = Mathf.Clamp(newPosition.y, -mapHeight, mapHeight);
+
+        rb.MovePosition(newPosition);
+    }
+}
