f6226ae399790b1612b34d606b9026e6e8d79eea
You Song Hou <yousonghou@yahoo.com>
2020-08-08 19:34:33 -0400

    add scripts to spawn fireball

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 6f;
+
+    void OnCollisionEnter2D(Collision2D  col)
+    {
+        if(col.collider.tag == "shield")
+        {
+            Debug.Log("shield");
+        }
+
+        if(col.collider.tag == "house")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void Update()
+    {
+        float x = Time.fixedDeltaTime * speed;
+
+        Vector2 newPosition = rb.position + Vector2.right * x;
+
+        rb.MovePosition(newPosition);
+    }
+}
---
+++ Assets/Scripts/FireballSpawn.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class FireballSpawn : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    public GameObject fireballPrefab;
+
+    public float timeBetweenWaves = 1f;
+
+    private float timeToSpawn = 3f;
+
+    void Update()
+    {
+        if(Time.time >= timeToSpawn)
+        {
+            SpawnBlocks();
+            
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+    }
+
+    // spawn fireball
+    void SpawnBlocks()
+    {
+        int randomFireball1 = Random.Range(0, spawnPoints.Length);
+        int randomFireball2 = Random.Range(0, spawnPoints.Length);
+        int randomFireball3 = Random.Range(0, spawnPoints.Length);
+
+        for(int i = 0; i < spawnPoints.Length; i++)
+        {
+            if(randomFireball1 == i || randomFireball2 == i || randomFireball3 == i)
+            {
+                Instantiate(fireballPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+            
+        }
+    }
+}
